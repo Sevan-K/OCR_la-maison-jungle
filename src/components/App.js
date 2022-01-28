dbe12343f5b4db5a7ca41162384a3db5bdf4e264
Sevan-K <sevan.kabakian@gmail.com>
2022-01-28 10:16:17 +0100

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// import du composant Banner
+import Banner from "./Banner";
+// import du composant Cart
+import Cart from "./Cart";
+// on créer le composant principal App
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+// on exporte App
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,6 @@
+// création du composant Banner
+function Banner() {
+  return <h1>La maison jungle</h1>;
+}
+// on exporte Le composant
+export default Banner;
